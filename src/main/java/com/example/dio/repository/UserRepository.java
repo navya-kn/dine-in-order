3977a99dda0aaae1698f510bdc0d38a9b757bd4a
navya <navya.nataraj.k.n@gmail.com>
2025-02-27 14:30:55 +0530

    repository created and updated

--- src/main/java/com/example/dio/repository/UserRepository.java
+++ src/main/java/com/example/dio/repository/UserRepository.java
@@ -1,4 +1,21 @@
 package com.example.dio.repository;
 
-public interface UserRepository {
+import com.example.dio.model.User;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.FluentQuery;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
+@Repository
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+
 }
