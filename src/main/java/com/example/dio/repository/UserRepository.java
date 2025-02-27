15a874cab1c9d5d43474c808a45286b1d444b511
navya <navya.nataraj.k.n@gmail.com>
2025-02-27 17:19:49 +0530

    created userByRole and mapToNewUser in UserServiceImpl

--- src/main/java/com/example/dio/repository/UserRepository.java
+++ src/main/java/com/example/dio/repository/UserRepository.java
@@ -1,17 +1,8 @@
 package com.example.dio.repository;
 
 import com.example.dio.model.User;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.FluentQuery;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
 
 @Repository
 
--- src/main/java/com/example/dio/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/dio/service/impl/UserServiceImpl.java
@@ -1,5 +1,10 @@
 package com.example.dio.service.impl;
 
+
+
+import com.example.dio.enums.UserRole;
+import com.example.dio.model.Admin;
+import com.example.dio.model.Staff;
 import com.example.dio.model.User;
 import com.example.dio.repository.UserRepository;
 import com.example.dio.service.UserService;
@@ -10,12 +15,40 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class UserServiceImpl implements UserService {
 
-
     private final UserRepository userRepository;
 
     @Override
     public User registerUser(User user) {
+        User user2 = this.createUserByRole(user.getRole());
 
-        return userRepository.save(user);
+        this.mapToNewUser(user,user2);
+        return userRepository.save(user2);
     }
+
+    @Override
+    public User getUser(User user) {
+        return null;
+    }
+
+    private User createUserByRole(UserRole role){
+        User user;
+        switch (role){
+            case ADMIN -> user = new Admin();
+            case STAFF -> user = new Staff();
+            default -> throw new RuntimeException("Failed to register user,invalid user Type!");
+        }
+        return  user;
+    }
+
+    private void mapToNewUser(User user,User user2){
+        user2.setUsername(user.getUsername());
+        user2.setEmail(user.getEmail());
+        user2.setPassword(user.getPassword());
+        user2.setRole(user.getRole());
+        user2.setPhno(user.getPhno());
+        user2.setCreateAt(user.getCreateAt());
+        user2.setLastModifiedAt(user.getLastModifiedAt());
 }
+}
+
+
