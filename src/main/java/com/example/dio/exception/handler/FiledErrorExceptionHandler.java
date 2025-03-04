48c31fab8660cd781fcfd71c5cb8dac49e825c52
navya <navya.nataraj.k.n@gmail.com>
2025-03-04 13:26:55 +0530

    modified pom.xml

---
+++ src/main/java/com/example/dio/exception/handler/FiledErrorExceptionHandler.java
@@ -0,0 +1,64 @@
+package com.example.dio.exception.handler;
+
+import com.example.dio.util.FieldErrorResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.example.dio.util.FieldErrorResponse.createFiledError;
+
+@RestControllerAdvice
+public class FiledErrorExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+//        List<FieldErrorResponse.CustomFieldError> errors = new ArrayList<>();
+//
+//        List<ObjectError> objectErrors = ex.getAllErrors();
+//
+//        for(ObjectError objectError : objectErrors){
+//            FieldError fieldError = (FieldError) objectError;
+//            FieldErrorResponse.CustomFieldError error = cerateFieldError(fieldError);
+//            errors.add(error);
+//
+//        }
+        List<FieldErrorResponse.CustomFieldError> errors =ex.getAllErrors().stream()
+                .map(error -> (FieldError)error)
+                .map(this::cerateFieldError)
+                .toList();
+        FieldErrorResponse error = createFieldErrorResponse(status, errors);
+
+
+
+
+
+        return ResponseEntity.status(status).body(error);
+    }
+
+
+    private static FieldErrorResponse createFieldErrorResponse(HttpStatusCode status, List<FieldErrorResponse.CustomFieldError> errors) {
+        return FieldErrorResponse.builder()
+                .type(status.toString())
+                .status(status.value())
+                .message("Invalid Input")
+                .errors(errors)
+                .build();
+    }
+    private FieldErrorResponse.CustomFieldError cerateFieldError (FieldError fieldError){
+        return  FieldErrorResponse.createFieldError(
+                fieldError.getDefaultMessage(),
+                fieldError.getRejectedValue(),
+                fieldError.getField());
+
+    }
+}
