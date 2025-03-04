8436086cbca565e1cb353a63ce9c929e78583055
navya <navya.nataraj.k.n@gmail.com>
2025-03-04 13:43:29 +0530

    added UserResponse file

--- src/main/java/com/example/dio/dto/response/UserResponse.java
+++ src/main/java/com/example/dio/dto/response/UserResponse.java
@@ -1,18 +1,18 @@
 package com.example.dio.dto.response;
 
 import com.example.dio.enums.UserRole;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 @Getter
+
 @Setter
-@Builder
+
+
 public class UserResponse {
-    private long userId;
+    private long userid;
     private String username;
     private UserRole role;
-    private LocalDateTime creatAt;
-    private LocalDateTime lastModifirdAt;
+    private LocalDateTime createAt;
+    private LocalDateTime lastModifiedAt;
 }
