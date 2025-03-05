7d41ffdec0cb5317c51b0b83ede5d843b5765805
navya <navya.nataraj.k.n@gmail.com>
2025-03-05 14:40:52 +0530

    adding documentations

--- src/main/java/com/example/dio/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/dio/service/impl/UserServiceImpl.java
@@ -21,6 +21,14 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
 
+    /**
+     *
+     * @param registrationRequest The request object containing the user details for registration,
+     *  *                            including fields such as username, email, password, etc.
+     *  * @return object containing the details of the newly registered user.
+     *  *         This response object includes relevant information about the user after the registration.
+     * @return
+     */
     @Override
     public UserResponse registerUser(RegistrationRequest registrationRequest) {
         User user = this.createUserByRole(registrationRequest.getRole());
@@ -33,6 +41,11 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    /**
+     * * @param userId The ID of the user to be retrieved.
+     *  * @return object containing the user's details.
+     *  * @throws UserNotFoundByIdException if no user is found with the given userId
+     */
     public UserResponse findUserById(long userId) {
 //        User user= userRepository.findById(userId).orElseThrow(()-> new UserNotFoundByIdException("Faild to find user"));
 //        return this.mapToUserResponse(user);
@@ -40,6 +53,14 @@ public class UserServiceImpl implements UserService {
                 .map(userMapper::mapToUserResponse)
                 .orElseThrow(()-> new UserNotFoundByIdException("Faild to find user,user not found by id"));
     }
+
+    /**
+     *Updates the details of an existing user identified by the provided userId
+     * @param userId userId The ID of the user to be updated. This should be a valid and existing user ID in the system.
+     * @param userRequest userRequest The request object containing the updated information for the user
+     * @return object containing the updated user information after the update is applied.
+     *          This response object is returned to the client.
+     */
 
     @Override
     public UserResponse updateUserById(long userId, UserRequest userRequest) {
@@ -51,7 +72,11 @@ public class UserServiceImpl implements UserService {
         return userMapper.mapToUserResponse(updatedUser);
     }
 
-
+    /**
+     * produces and returns child instance of the User based on the user role
+     * @param role role of the user
+     * @return User the parent reference containing either of staff or admin instance
+     */
 
     private User createUserByRole(UserRole role){
         User user;
